de217a86b958eedfe67bb8c8cce19bffadc1a60d
lexxai <lexxai@users.noreply.github.com>
2022-11-28 03:55:59 +0200

    Update main.py

--- MiTermometerPVVX/main.py
+++ MiTermometerPVVX/main.py
@@ -14,9 +14,9 @@ async def main():
         name=advertising_data.local_name
         if name and name[0:3]=="ATC" :
             adv_atc=advertising_data.service_data['0000181a-0000-1000-8000-00805f9b34fb']
-            count=int.from_bytes(adv_atc[13:14], byteorder='little', signed=False) 
+            count=int.from_bytes(adv_atc[13:14], byteorder='little', signed=False)
             if atc_counters.get(name) != count :
-                atc_counters.update({name: count}) 
+                atc_counters.update({name: count})
                 date_now=datetime.datetime.now()
                 date_prev=atc_date.get(name)
                 if date_prev:
@@ -24,12 +24,12 @@ async def main():
                     date_diff=datetime.timedelta(seconds=round(date_diff.total_seconds()))
                 else:
                     date_diff=0
-                atc_date.update({name: date_now}) 
+                atc_date.update({name: date_now})
                 temp=int.from_bytes(adv_atc[6:8], byteorder='little', signed=True)/100.0
                 humidity=int.from_bytes(adv_atc[8:10], byteorder='little', signed=True)/100.0
                 battery_v=int.from_bytes(adv_atc[10:12], byteorder='little', signed=False)
-                battery=int.from_bytes(adv_atc[12:13], byteorder='little', signed=False)    
-                #flag=int.from_bytes(adv_atc[14:15], byteorder='little', signed=False) 
+                battery=int.from_bytes(adv_atc[12:13], byteorder='little', signed=False)
+                #flag=int.from_bytes(adv_atc[14:15], byteorder='little', signed=False)
                 print()
                 print("device:\t ", name)
                 name_len=len(str(name))+10
