05ac577920d56609ade25c54b2099a2184a38146
lexxai <lexxai@users.noreply.github.com>
2022-11-28 03:42:08 +0200

    Update main.py

--- MiTermometerPVVX/main.py
+++ MiTermometerPVVX/main.py
@@ -2,34 +2,34 @@ import asyncio
 import datetime
 from bleak import BleakScanner
 
-#MiTermometerPVVX"
+# MiTermometerPVVX
 
 async def main():
     print("Scanning BLE devices of type 'ATC_MiThermometer (PVVX)', please wait...")
     stop_event = asyncio.Event()
-    ATC_COUNTERS={};
-    ATC_DATE={};
+    atc_counters={}
+    atc_date={}
 
-    def callback(device, advertising_data):
+    def callback(_device, advertising_data):
         name=advertising_data.local_name;
         if name and name[0:3]=="ATC" :
-            advatc=advertising_data.service_data['0000181a-0000-1000-8000-00805f9b34fb']
-            count=int.from_bytes(advatc[13:14], byteorder='little', signed=False) 
-            if ATC_COUNTERS.get(name) != count :
-                ATC_COUNTERS.update({name: count}) 
+            adv_atc=advertising_data.service_data['0000181a-0000-1000-8000-00805f9b34fb']
+            count=int.from_bytes(adv_atc[13:14], byteorder='little', signed=False) 
+            if atc_counters.get(name) != count :
+                atc_counters.update({name: count}) 
                 datenow=datetime.datetime.now()
-                dateprev=ATC_DATE.get(name)
+                dateprev=atc_date.get(name)
                 if dateprev:
                     datediff=datenow-dateprev
                     datediff=datetime.timedelta(seconds=round(datediff.total_seconds()))
                 else:
                     datediff=0
-                ATC_DATE.update({name: datenow}) 
-                temp=int.from_bytes(advatc[6:8], byteorder='little', signed=True)/100.0
-                humidity=int.from_bytes(advatc[8:10], byteorder='little', signed=True)/100.0
-                batteryv=int.from_bytes(advatc[10:12], byteorder='little', signed=False)
-                battery=int.from_bytes(advatc[12:13], byteorder='little', signed=False)    
-                flag=int.from_bytes(advatc[14:15], byteorder='little', signed=False) 
+                atc_date.update({name: datenow}) 
+                temp=int.from_bytes(adv_atc[6:8], byteorder='little', signed=True)/100.0
+                humidity=int.from_bytes(adv_atc[8:10], byteorder='little', signed=True)/100.0
+                batteryv=int.from_bytes(adv_atc[10:12], byteorder='little', signed=False)
+                battery=int.from_bytes(adv_atc[12:13], byteorder='little', signed=False)    
+                #flag=int.from_bytes(adv_atc[14:15], byteorder='little', signed=False) 
                 print()
                 print("device:\t ", name)
                 namelen=len(str(name))+10
@@ -46,9 +46,9 @@ async def main():
                 print(f'time now: {datenow.strftime("%H:%M:%S")}{datedifftext}')
 
     try:
-        async with BleakScanner(callback) as scanner:
+        async with BleakScanner(callback):
             await stop_event.wait()
-    except asyncio.CancelledError as ex:
+    except asyncio.CancelledError:
         print('**** task scanner cancelled')
         stop_event.set()
 
