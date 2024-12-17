b1d3310253c30c58b503fb9e4b18b3d4a496cb97
lexxai <lexxai@gmail.com>
2024-12-17 03:56:48 +0200

    add docstrings

--- MiTermometerPVVX/notifications.py
+++ MiTermometerPVVX/notifications.py
@@ -1,6 +1,6 @@
 import asyncio
-from abc import ABC, abstractmethod
 import logging
+from abc import ABC, abstractmethod
 from typing import Protocol, TypeVar
 
 from utils import AsyncWithDummy
@@ -12,34 +12,95 @@ logger = logging.getLogger(f"BLEScanner.{__name__}")
 
 
 class NotificationAbstract(ABC):
+    """
+    Abstract base class for notifications.
+
+    Provides a blueprint for notification classes with a lock property
+    and an abstract method to send alert messages.
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes the NotificationAbstract class.
+        """
         super().__init__()
 
     @property
     def lock(self) -> AsyncWithDummy:
+        """
+        Returns a lock object for use when sending notifications.
+
+        Returns:
+            AsyncWithDummy: An asyncio lock.
+        """
         return AsyncWithDummy()
 
     @abstractmethod
     async def send_alert(
         self, title: str | None = None, message: str | None = None
     ) -> None:
-        """Sends an alert message."""
+        """
+        Sends an alert message.
+
+        Args:
+            title (str | None): The title of the notification, if provided.
+            message (str | None): The message content of the notification, if provided.
+        """
         ...
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the class.
+
+        Returns:
+            str: The class name in lowercase without 'Notification'.
+        """
         return f"{self.__class__.__name__.split('Notification')[0].lower() or self.__class__.__name__}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the class for debugging.
+
+        Returns:
+            str: The class name in lowercase without 'Notification'.
+        """
         return self.__str__()
+
+    """
+    A Notification class that logs messages to the logger.
+
+    This class logs message with the INFO level and can be used to log messages
+    without sending any notifications.
+
+    Attributes:
+        _lock (asyncio.Lock | AsyncWithDummy): The lock to use when sending
+            notifications. If not provided, a dummy lock is used.
+    """
 
 
 class LoggerNotification(NotificationAbstract):
     def __init__(self, lock: asyncio.Lock = None) -> None:
+        """
+        Initializes the LoggerNotification object.
+
+        Args:
+            lock (asyncio.Lock | AsyncWithDummy): The lock to use when sending
+                notifications. If not provided, a dummy lock is used.
+        """
         super().__init__()
         self._lock = lock
 
     @property
     def lock(self) -> asyncio.Lock | AsyncWithDummy:
+        """
+        Returns the lock object for use when sending notifications.
+
+        If no lock is provided, a dummy lock is used.
+
+        Returns:
+            asyncio.Lock | AsyncWithDummy: The lock object to use when sending
+                notifications.
+        """
         return self._lock or AsyncWithDummy()
 
     # @staticmethod
@@ -154,16 +215,41 @@ class ManagerAbstract:
     """
 
     def __init__(self, tasks: list[T]):
+        """Initialize ManagerAbstract with a list of tasks.
 
+        Args:
+            tasks (list[T]): A list of notification tasks.
+
+        """
         self.tasks = tasks or []
 
     def get(self) -> list[T]:
+        """Get the list of tasks.
+
+        Returns:
+            list[T]: The list of tasks.
+
+        """
         return self.tasks
 
     def register(self, task: T):
+        """Register a task.
+
+        Args:
+            task (T): The task to be registered.
+
+        """
         self.tasks.append(task)
 
     def unregister(self, name: str | None = None, task: T = None):
+        """Unregister a task.
+
+        Args:
+            name (str | None): The name of the task to be unregistered.
+                If None, the task object must be provided.
+            task (T): The task object to be unregistered.
+
+        """
         if task:
             self.tasks.remove(task)
             return
@@ -175,6 +261,16 @@ class ManagerAbstract:
                 break
 
     def filter(self, name: list[str] | None, inplace: bool = True) -> list[T] | None:
+        """
+        Filter tasks by their names.
+
+        Args:
+            name (list[str] | None): List of task names to filter by.
+            inplace (bool): If True, update the current task list in place.
+
+        Returns:
+            list[T] | None: The filtered list of tasks, or None if no tasks exist.
+        """
         if name:
             tasks = list(filter(lambda n: str(n) in name, self.tasks))
             if inplace:
@@ -184,6 +280,12 @@ class ManagerAbstract:
         return self.tasks or None
 
     def get_names(self) -> list[str]:
+        """
+        Retrieve the names of all registered tasks.
+
+        Returns:
+            list[str]: A list of task names.
+        """
         return [str(n) for n in self.tasks]
 
 
