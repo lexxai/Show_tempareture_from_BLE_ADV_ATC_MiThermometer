403baa188e2e894eea0d2ef8f2b1ab892f343ba7
lexxai <lexxai@gmail.com>
2024-12-18 05:19:43 +0200

    detect self.on_platform

--- MiTermometerPVVX/notifications.py
+++ MiTermometerPVVX/notifications.py
@@ -14,6 +14,138 @@ from discord_api import send_message as discord_send_message
 logger = logging.getLogger(f"BLEScanner.{__name__}")
 
 
+# ---------------------------------------------
+
+T = TypeVar("T", bound="TaskProtocol")
+
+
+class TaskProtocol(Protocol):
+    """Base class for notification tasks.
+
+    Defines the interface for a notification task, providing a common API for
+    different notification types.
+
+    """
+
+    def __str__(self) -> str: ...
+
+    def __repr__(self) -> str: ...
+
+    async def send_alert(
+        self, title: str | None = None, message: str | None = None
+    ) -> None: ...
+
+    @property
+    def lock(self) -> None | asyncio.Lock: ...
+
+
+class ManagerAbstract:
+    """Abstract class to manage notification tasks.
+
+    Provides common methods for registration, un-registration, filtering and
+    retrieval of tasks.
+
+    """
+
+    def __init__(self, tasks: list[T]):
+        """Initialize ManagerAbstract with a list of tasks.
+
+        Args:
+            tasks (list[T]): A list of notification tasks.
+
+        """
+        self.tasks = tasks or []
+
+    def get(self) -> list[T]:
+        """Get the list of tasks.
+
+        Returns:
+            list[T]: The list of tasks.
+
+        """
+        return self.tasks
+
+    def register(self, task: T):
+        """Register a task.
+
+        Args:
+            task (T): The task to be registered.
+
+        """
+        self.tasks.append(task)
+
+    def unregister(self, name: str | None = None, task: T = None):
+        """Unregister a task.
+
+        Args:
+            name (str | None): The name of the task to be unregistered.
+                If None, the task object must be provided.
+            task (T): The task object to be unregistered.
+
+        """
+        if task:
+            self.tasks.remove(task)
+            return
+        if not name:
+            return
+        for n in self.tasks:
+            if str(n) == name:
+                self.tasks.remove(n)
+                break
+
+    def filter(self, name: list[str] | None, inplace: bool = True) -> list[T] | None:
+        """
+        Filter tasks by their names.
+
+        Args:
+            name (list[str] | None): List of task names to filter by.
+            inplace (bool): If True, update the current task list in place.
+
+        Returns:
+            list[T] | None: The filtered list of tasks, or None if no tasks exist.
+        """
+        if name:
+            tasks = list(filter(lambda n: str(n) in name, self.tasks))
+            if inplace:
+                self.tasks = tasks
+            else:
+                return tasks
+        return self.tasks or None
+
+    def get_names(self) -> list[str]:
+        """
+        Retrieve the names of all registered tasks.
+
+        Returns:
+            list[str]: A list of task names.
+        """
+        return [str(n) for n in self.tasks]
+
+
+class ManagerNotifications(ManagerAbstract):
+
+    async def send_alert(
+        self, title: str | None = None, message: str | None = None
+    ) -> None:
+        """
+        Sends an alert message to all registered notification tasks.
+
+        Args:
+            title (str | None): The title of the notification, if provided.
+            message (str | None): The message content of the notification, if provided.
+
+        Returns:
+            None
+        """
+        if not self.tasks:
+            return
+        for n in self.tasks:
+            await n.send_alert(title, message)
+
+
+# ==========================================================
+
+
 class NotificationAbstract(ABC):
     """
     Abstract base class for notifications.
@@ -170,7 +302,8 @@ class DiscordNotification(NotificationAbstract):
 class SystemNotification(NotificationAbstract):
     def __init__(self, timeout: int = None):
         super().__init__()
-        self.timeout = timeout or 30
+        self.on_platform = getattr(notification, "notify", None)
+        self.timeout = timeout or 1
         self.icon = str(settings.BASE_PATH.parent.joinpath("icon-64x64.ico"))
         self.app_name = settings.APP_NAME
 
@@ -178,140 +311,41 @@ class SystemNotification(NotificationAbstract):
         self, title: str | None = None, message: str | None = None
     ) -> None:
         """Sends an alert message."""
-        logger.debug("*** START SYSTEM NOTIFICATION ***")
-        await asyncio.to_thread(
+        if not self.on_platform:
+            logger.warning("SYSTEM NOTIFICATION is not available on this platform.")
+            return
+        # logger.debug("*** START SYSTEM NOTIFICATION ***")
+        coro = asyncio.to_thread(
             notification.notify,
             title=title,
             message=message,
             timeout=self.timeout,
             app_name=self.app_name,
             app_icon=self.icon,
+            ticker=title,
         )
-        logger.debug("*** END SYSTEM NOTIFICATION ***")
+        asyncio.create_task(coro)
+        # logger.debug("*** END SYSTEM NOTIFICATION ***")
 
 
-T = TypeVar("T", bound="TaskProtocol")
+class VoiceNotification(NotificationAbstract):
 
-
-class TaskProtocol(Protocol):
-    """Base class for notification tasks.
-
-    Defines the interface for a notification task, providing a common API for
-    different notification types.
-
-    """
-
-    def __str__(self) -> str: ...
-
-    def __repr__(self) -> str: ...
-
-    async def send_alert(
-        self, title: str | None = None, message: str | None = None
-    ) -> None: ...
-
-    @property
-    def lock(self) -> None | asyncio.Lock: ...
-
-
-class ManagerAbstract:
-    """Abstract class to manage notification tasks.
-
-    Provides common methods for registration, un-registration, filtering and
-    retrieval of tasks.
-
-    """
-
-    def __init__(self, tasks: list[T]):
-        """Initialize ManagerAbstract with a list of tasks.
-
-        Args:
-            tasks (list[T]): A list of notification tasks.
-
-        """
-        self.tasks = tasks or []
-
-    def get(self) -> list[T]:
-        """Get the list of tasks.
-
-        Returns:
-            list[T]: The list of tasks.
-
-        """
-        return self.tasks
-
-    def register(self, task: T):
-        """Register a task.
-
-        Args:
-            task (T): The task to be registered.
-
-        """
-        self.tasks.append(task)
-
-    def unregister(self, name: str | None = None, task: T = None):
-        """Unregister a task.
-
-        Args:
-            name (str | None): The name of the task to be unregistered.
-                If None, the task object must be provided.
-            task (T): The task object to be unregistered.
-
-        """
-        if task:
-            self.tasks.remove(task)
-            return
-        if not name:
-            return
-        for n in self.tasks:
-            if str(n) == name:
-                self.tasks.remove(n)
-                break
-
-    def filter(self, name: list[str] | None, inplace: bool = True) -> list[T] | None:
-        """
-        Filter tasks by their names.
-
-        Args:
-            name (list[str] | None): List of task names to filter by.
-            inplace (bool): If True, update the current task list in place.
-
-        Returns:
-            list[T] | None: The filtered list of tasks, or None if no tasks exist.
-        """
-        if name:
-            tasks = list(filter(lambda n: str(n) in name, self.tasks))
-            if inplace:
-                self.tasks = tasks
-            else:
-                return tasks
-        return self.tasks or None
-
-    def get_names(self) -> list[str]:
-        """
-        Retrieve the names of all registered tasks.
-
-        Returns:
-            list[str]: A list of task names.
-        """
-        return [str(n) for n in self.tasks]
-
-
-class ManagerNotifications(ManagerAbstract):
+    def __init__(self):
+        super().__init__()
+        self.on_platform = getattr(notification, "speak", None)
 
     async def send_alert(
         self, title: str | None = None, message: str | None = None
     ) -> None:
-        """
-        Sends an alert message to all registered notification tasks.
-
-        Args:
-            title (str | None): The title of the notification, if provided.
-            message (str | None): The message content of the notification, if provided.
-
-        Returns:
-            None
-        """
-        if not self.tasks:
+        """Sends an alert message."""
+        if not self.on_platform:
+            logger.warning("VOICE NOTIFICATION is not available on this platform.")
             return
-        for n in self.tasks:
-            await n.send_alert(title, message)
+
+        # logger.debug("*** START VOICE NOTIFICATION ***")
+        coro = asyncio.to_thread(
+            notification.speak,
+            message=message,
+        )
+        asyncio.create_task(coro)
+        # logger.debug("*** END VOICE NOTIFICATION ***")
