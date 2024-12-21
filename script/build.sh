824af43e38257f0448197a2c09c86bdc1043e648
lexxai <lexxai@gmail.com>
2024-12-21 23:25:40 +0200

    pyinstaller --collect-all pync

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,7 @@
 RUNNER="poetry run"
 
 $RUNNER python ./build_version.py
+ENVFILE="../.env"
 
 rm -R "../pyinstall" 
 mkdir -p "../pyinstall"
@@ -11,5 +12,10 @@ pushd "../pyinstall"
 
 export PYTHONPATH=../MiTermometerPVVX
 echo $PYTHONPATH
-$RUNNER pyinstaller --clean ../MiTermometerPVVX/main.py --name MiTermometerPVVX --onefile -w -c
+$RUNNER pyinstaller --clean ../MiTermometerPVVX/main.py --collect-all pync --name MiTermometerPVVX --onefile -w -c
+
+if [ -f "${ENVFILE}" ] && [ -d "dist" ] ; then
+    echo "Copy ${ENVFILE} to dist/"
+    cp "${ENVFILE}" dist/
+fi 
 popd
